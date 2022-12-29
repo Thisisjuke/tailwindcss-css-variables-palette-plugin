37e0ae5b69658a29df15ff480fe68c85e4898af6
Phatify <70096611+phatify@users.noreply.github.com>
2022-12-29 19:07:03 +0700

    refactor the output of the color token

--- src/utils/tokens.js
+++ src/utils/tokens.js
@@ -5,7 +5,13 @@ export const generateColorTokens = (name, palette) => {
     const hex = palette[key]
     const cssToken = key === 'DEFAULT' ? `--color-${name}`: `--color-${name}-${key}`
 
-    colors[key] = `var(${cssToken}, ${hex})`
+    // colors[key] = `var(${cssToken}, ${hex})`; 
+    // generated output: --color-primary: var(--color-primary, #xxxxxx); 
+    // shouldn't be like that because you can't reusable these variables (e.g: backgroundColor: 'var(--color-primary)')
+    
+    colors[key] = `${hex}`; 
+    // generated output: --color-primary: #xxxxxx; 
+    // everything is ok
   });
 
   return {
